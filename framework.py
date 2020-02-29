8c1207f363e431adff36c7872f513b4005d95717
Jacob <jacobmen416@yahoo.com>
2020-02-29 12:04:21 -0600

    setup routing for tutorials

--- framework.py
+++ framework.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from config import DevelopmentConfig
 
 app = Flask(__name__)
+# TODO: Change config for deployment later
+app.config.from_object(DevelopmentConfig)
 
 if __name__ == "__main__":
     app.run()
@@ -9,3 +12,11 @@ if __name__ == "__main__":
 @app.route("/")
 def landing_site():
     return render_template("fintech.html")
+
+@app.route("/one-part-ddm")
+def one_part_ddm():
+    return "TEMP LANDING"
+
+@app.route("/two-part-ddm")
+def two_part_ddm():
+    return "TEMP LANDING"
